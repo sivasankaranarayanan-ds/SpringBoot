8ee9d692a30f187ad39a5a25f4ef889c7fea1c0d
sivasankaranarayanan-ds <sivasankaranarayanan.k@datasirpi.com>
2024-02-01 14:52:49 +0530

    spring-cloud

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/microservices/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,30 @@
+package microservices.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MovieCatalogServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}
